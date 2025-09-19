04711f30142534c974d33a64c5520ae835d1b706
AdlanAdi <na_adi@esi.dz>
2025-09-19 20:11:22 +0100

    .

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
+import (
+    "crypto/md5"
+    "fmt"
+)
 
 func main() {
 	// main is empty on purpose
+	password := "supersecret"
+    hash := md5.Sum([]byte(password)) // gosec will flag MD5 as weak crypto
+    fmt.Printf("%x\n", hash)
 }
